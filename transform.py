bbf4948f84827ae2bc24f2dee87ad3d32290278b
ADMIN <pivar999@gmail.com>
2024-12-24 20:00:01 +0700

    cân bằng dữ liệu

--- transform.py
+++ transform.py
@@ -15,7 +15,26 @@ class PreProcessing:
         self.comment = comment
 
     def remove_icon(self):
-        self.comment = re.sub(r'[^\w\s,.!@#$%^&*()_+=\-\[\]{};:"\'|\\<>/?`~\u00C0-\u1EF9]', ' ', self.comment)
+        emoji_pattern = re.compile("["
+                                   u"\U0001F600-\U0001F64F"
+                                   u"\U0001F300-\U0001F5FF"
+                                   u"\U0001F680-\U0001F6FF"
+                                   u"\U0001F1E0-\U0001F1FF"
+                                   u"\u2702-\u27B0"
+                                   u"\u24C2-\U0001F251"
+                                   u"\U0001f926-\U0001f937"
+                                   u'\U00010000-\U0010ffff'
+                                   u"\u200d"
+                                   u"\u2640-\u2642"
+                                   u"\u2600-\u2B55"
+                                   u"\u23cf"
+                                   u"\u23e9"
+                                   u"\u231a"
+                                   u"\u3030"
+                                   u"\ufe0f"
+                                   "]+", flags=re.UNICODE)
+
+        self.comment = emoji_pattern.sub(' ', self.comment)
         return self
 
     def lowercase(self):
